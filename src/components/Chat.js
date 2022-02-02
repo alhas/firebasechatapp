6aaf4944c1ac754ce27b05acceb44d73945e1e0e
alhas <bahtiyaralialhas@gmail.com>
2022-02-02 19:49:56 +0100

    works

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { db } from '../firebase';
+import React, { useState, useEffect, useRef } from 'react';
+import { db, auth } from '../firebase';
 import SignOut from './SignOut';
 import { collection, orderBy, limit, onSnapshot, getDocs, query } from 'firebase/firestore'
 import SendMessage from './SendMessage';
 
 function Chat() {
-
+    const scroll = useRef()
     const [messages, setMessages] = useState([])
 
     useEffect(() => {
 
         const collectionReg = collection(db, 'messages');
-        const q = query(collectionReg, orderBy('createdAt', 'desc'),limit(50))
+        const q = query(collectionReg, orderBy('createdAt', 'asc'), limit(50))
 
         const unsub = onSnapshot(q, (querySnapshot) => {
             setMessages(querySnapshot.docs.map(doc => ({
-
                 id: doc.id,
                 data: doc.data()
             })))
@@ -30,15 +29,18 @@ function Chat() {
 
         <div>
             <SignOut />
-            {console.log(messages)}
-
-            {messages.map(({ id, data, photoURL }) => (
-                <div key={id}>
-                    <img src={photoURL} alt='' />
-                    <p>{data.text}</p>
-                </div>
-            ))}
-            <SendMessage />
+            <div className="msgs">
+                {messages.map(({ id, data }) => (
+                    <div>
+                        <div key={id} className={`msg ${data.uid === auth.currentUser.uid ? 'sent' : 'received'}`}>
+                            <img src={data.photoURL} alt='' />
+                            <p>{data.text}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <SendMessage scroll={scroll} />
+            <div ref={scroll}></div>
         </div>
 
 
--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -4,7 +4,7 @@ import { db, auth } from '../firebase'
 import { addDoc, collection, serverTimestamp } from "firebase/firestore"
 
 
-function SendMessage() {
+function SendMessage({ scroll }) {
 
     const [msg, setMsg] = useState('')
 
@@ -19,15 +19,17 @@ function SendMessage() {
             createdAt: serverTimestamp()
         })
         setMsg('')
+        scroll.current.scrollIntoView({ behaviour: 'smooth' })
     }
 
     return <div>
 
         <form onSubmit={SendMessage}>
 
-            <Input value={msg} onChange={(e) => setMsg(e.target.value)} placeholder="" />
-            <Button type='submit'>Send</Button>
-
+            <div className="sendMsg">
+                <Input style={{ width: '78%', fontSize: '15px', fontWeight: '550', marginLeft: '5px', marginBottom: '-3px' }} placeholder='Message...' type="text" value={msg} onChange={e => setMsg(e.target.value)} />
+                <Button style={{ width: '18%', fontSize: '15px', fontWeight: '550', margin: '4px 5% -13px 5%', maxWidth: '200px' }} type="submit">Send</Button>
+            </div>
         </form>
 
     </div >;
